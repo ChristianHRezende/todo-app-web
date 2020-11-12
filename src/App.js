3ebd25da7b4372a3b79528e31ed6eaed09177365
Christian <christian_srs_@hotmail.com>
2020-11-12 19:56:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Butao from './Butao'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Um teste legal</h1>
+      <Butao isViado={true}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Butao.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Butao = (props) => {
+    console.log('é viado? ====== ',props.isViado) 
+    return(
+        <button type="button">CLICA NESSA PORRA</button>
+    )
+}
+
+export default Butao
